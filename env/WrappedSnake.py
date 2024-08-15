dfa6f81f9ef32638ad2273d1bf2b8ec24bbf3f4f
mostmosthandsome <rgzsc246@126.com>
2024-08-15 12:57:43 +0800

    add get_obs and get avail action

--- env/WrappedSnake.py
+++ env/WrappedSnake.py
@@ -2,11 +2,13 @@ import sys
 import os
 sys.path.append(os.path.dirname(__file__))
 from snakes import SnakeEatBeans
+import numpy as np
 
 class SnakeEnv:
     def __init__(self,config:dict):
         self.snake_env = SnakeEatBeans(config)
         self.episode_limit = self.snake_env.max_step
+        self.n_agents = self.snake_env.n_player
 
 
     def get_env_info(self):
@@ -14,6 +16,18 @@ class SnakeEnv:
             "state_shape": 6 * (7 + 4),
             "obs_shape": 6 * (7 + 4),
             "n_actions": self.snake_env.n_player,
-            "n_agents": self.snake_env.n_player,
+            "n_agents": self.n_agents,
             "episode_limit": self.snake_env.max_step,
         }
+
+    def reset(self):
+        self.snake_env.reset(render=True)
+
+    def get_state(self):
+        return self.snake_env.get_all_observes()
+
+    def get_avail_actions(self):
+        return [[-2, 2, -1, 1]for i in range(self.n_agents)]
+
+    def get_obs(self):
+        return self.snake_env.get_all_observes()
