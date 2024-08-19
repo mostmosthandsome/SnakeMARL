6c30bb2f1891a454a39b4146ed5a1b3e36c9e9fe
mostmosthandsome <rgzsc246@126.com>
2024-08-19 21:13:36 +0800

    stable release

--- env/WrappedSnake.py
+++ env/WrappedSnake.py
@@ -4,52 +4,88 @@ sys.path.append(os.path.dirname(__file__))
 from snakes import SnakeEatBeans
 import numpy as np
 from multiagentenv import MultiAgentEnv
-import torch as th
+from submissions.blue import policy as blue_policy
 
 class SnakeEnv(MultiAgentEnv):
     """
-    引用自
+    引用自smac库的mutltiagent
     """
-    def __init__(self,config:dict):
+    def __init__(self,config:dict,render = True):
         self.snake_env = SnakeEatBeans(config)
         self.episode_limit = self.snake_env.max_step
         self.n_agents = self.snake_env.n_player
         self.n_controlled_player = 3
         self.sight_range = self.snake_env.sight_range
         self.n_beans = self.snake_env.n_beans
-        self.obs_dim = self.sight_range * self.sight_range * 4 + self.n_beans * 2 + 3
-
+        self.obs_dim = self.sight_range * self.sight_range * 4 + self.n_beans * 2 + 2 + self.n_controlled_player * 2
+        self.current_obs = None
+        self.action_index_to_value = np.eye(4)
+        self.action_value_to_index = {-2:0,-1:1,1:2,2:3}
+        self.render = render
 
     def get_env_info(self):
         return {
-            "state_shape": 6 * (7 + 4),
-            "obs_shape": 6 * (7 + 4),
-            "n_actions": self.snake_env.n_player,
-            "n_agents": self.n_agents,
+            "state_shape": 3 * self.obs_dim,
+            "obs_shape": self.obs_dim,
+            "n_actions": 4,
+            "n_agents": self.n_controlled_player,
             "episode_limit": self.snake_env.max_step,
         }
 
     def reset(self):
-        self.snake_env.reset(render=True)
+        self.snake_env.reset(render=self.render)
 
     def get_state(self):
-        return self.snake_env.get_all_observes()
+        return np.array(self.get_obs()[:3]).reshape(-1)
+        # obs = self.snake_env.get_all_observes()
 
+    #
     def get_avail_actions(self):
-        return [[-2, 2, -1, 1]for i in range(self.n_agents)]
+        """
+        能动的方向值为1,不能动的方向值为0
+        """
+        self.current_obs = self.snake_env.get_all_observes()
+        avail_action = []
+        for i in range(3):
+            avail_action.append(self.get_avail_agent_actions(i))
+        return avail_action
+
+    def get_avail_agent_actions(self, agent_id):
+        avail_direction = [1,1,1,1]#按顺序排-2,-1,1,2，对应
+        last_dir = self.current_obs[agent_id]['last_direction']
+        if last_dir is None:
+            return avail_direction
+        avail_direction[self.action_value_to_index[last_dir]] = 0
+        return avail_direction
+
 
     def get_obs(self):
         obs = self.snake_env.get_all_observes()
-        beans_pos = th.tensor(obs[0][1]).flatten(start_dim=0)
-        board_width = th.tensor(obs[0]['board_width'])
+        beans_pos = np.array(obs[0][1]).reshape(-1)
+        board_width = obs[0]['board_width']
+        board_height = obs[0]['board_height']
+        new_obs_list = []
+
         for i in range(self.n_controlled_player):
             single_obs = obs[i]
             snake_head = single_obs[i + 2][0]
-            snake_grid = th.ones([self.sight_range * 2,self.sight_range * 2]) * -1
+            snake_grid = np.ones([self.sight_range * 2,self.sight_range * 2]) * -1
             for j in range(self.n_agents):
                 for coordinate in single_obs[j + 2]:
                     if pow(coordinate[0] - snake_head[0],2) + pow(coordinate[1] - snake_head[1],2) <= self.sight_range * self.sight_range:
                         snake_grid[coordinate[0] - snake_head[0]][coordinate[1] - snake_head[1]] = j
+            modified_obs = np.concatenate([snake_grid.flatten(),beans_pos])
+            modified_obs = np.concatenate([modified_obs, [board_width, board_height]])
+            for j in range(3):#append allies' head
+                modified_obs = np.concatenate([modified_obs,single_obs[j + 2][0]])
+            new_obs_list.append(modified_obs)
+        return new_obs_list
 
-        return th.cat([snake_grid,beans_pos,board_width],dim=1)
-
+    def step(self, actions):
+        red_action = [[self.action_index_to_value[actions[i]].tolist()] for i in range(3)]
+        blue_action = blue_policy(self.current_obs[3:])
+        all_action = red_action + blue_action
+        obs, rewards, terminal, info = self.snake_env.step(all_action)
+        snake_length = [len(self.snake_env.players[i].segments) for i in range(3)]
+        all_reward = np.sum(rewards[:3]) + np.max(snake_length) - 3
+        return all_reward,terminal,info
